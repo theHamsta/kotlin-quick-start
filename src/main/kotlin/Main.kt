3e88b5d523bcf54eb2ee12235a78618d9e00c42b
Stephan Seitz <stephan.seitz@fau.de>
2019-12-07 20:00:36 +0100

    Use ImageJ

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
+import ij.ImageJ
+
+data class Foo(var a: String, var b: Int)
+
 fun main(args: Array<String>) {
 	println("Hello world")
+	var f = Foo("hallo", 3)
+	var number  = f.b 
+
+	println(f)
+	println(number)
+
+	ImageJ()
 }
